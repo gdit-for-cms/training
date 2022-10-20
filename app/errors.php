75087659ec75c7aaff6d4f0ae75fe42b15d7e2ec
GLODEIT\Phong.HQ <phong25t25t@gmail.com>
2022-10-20 13:54:15 +0700

    feature (helper, responseTrait, errorHandler)

---
+++ app/errors.php
@@ -0,0 +1,17 @@
+<?php
+
+function showError($errorName)
+{
+    $errorArray = [
+        "login" => 'Email or Password is incorrect',
+        "create" => 'Create failed',
+        "existed" => 'Object has been exist',
+        "email existed" => 'Email has been exist',
+        "undefindError" => "Error undefind",
+    ];
+    if ($errorArray[$errorName]) {
+        return $errorArray[$errorName];
+    } else {
+        return  $errorArray['undefindError'];
+    }
+}
