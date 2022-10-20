ecac3ab0609af1e84c73699ceebc77d14456a045
GLODEIT\Phong.HQ <phong25t25t@gmail.com>
2022-10-20 10:15:41 +0700

    update (admin master-layouts)

---
+++ app/views/admin/back-layouts/sidebar.php
@@ -0,0 +1,31 @@
+<nav class="sidebar">
+    <div class="logo d-flex justify-content-between">
+        <a class="large_logo" href="/admin/dashboard"><img src="" alt=""></a>
+        <a class="small_logo" href="/admin/dashboard"><img src="" alt=""></a>
+        <div class="sidebar_close_icon d-lg-none">
+            <i class="ti-close"></i>
+        </div>
+    </div>
+    <ul id="sidebar_menu" class="metismenu">
+        <li class="">
+            <a class="has-arrow" href="/user/index" aria-expanded="false">
+                <div class="nav_icon_small">
+                    <img src="" alt="">
+                </div>
+                <div class="nav_title">
+                    <span>User</span>
+                </div>
+            </a>
+        </li>
+        <li class="">
+            <a class="has-arrow" href="/admin/diff" aria-expanded="false">
+                <div class="nav_icon_small">
+                    <img src="" alt="">
+                </div>
+                <div class="nav_title">
+                    <span>Diff-file</span>
+                </div>
+            </a>
+        </li>
+    </ul>
+</nav>
