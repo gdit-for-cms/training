e118d7738a674ba4eace45d05f5c3accb53bfa01
GLODEIT\Phu.TB <phu.tb@globaldesignit.vn>
2023-06-06 15:05:38 +0700

    Last update Selenium screenshot

--- README.md
+++ README.md
@@ -1,11 +1,13 @@
 Run composer install
+Run composer require facebook/webdriver
+Download ChromeDriver tương thích với Google chrome qua trang web:https://chromedriver.chromium.org/home
+Unzip path/to/chromedriver.zip 
+Run sudo cp path/to/chromedriver /usr/local/bin/ ---> Sao chép tệp vừa unzip vào /usr/local/bin/
+Run export PATH=$PATH:/usr/local/bin ---> Add vào tệp ~/.bashrc hoặc ~/.bash_profile
+Run source ~/.bashrc hoặc source ~/.bash_profile ---> Cập nhật biến môi trường PATH
+Run chromedriver --port=5555 ---> Khởi chạy chromedriver
+Run sudo yum install java-1.8.0-openjdk ---> Cài đặt môi trường java để khởi chạy selenium
+Download selenium-server-4.9.1 qua trang web:https://www.selenium.dev/downloads/
+Run java -jar selenium-server-4.9.1.jar standalone ---> Khởi chạy selenium và bất đầu chụp màng hình
 
-Run php composer.phar init
-
-Run php composer.phar require php-webdriver/webdriver
-
-Run sudo yum install java-1.8.0-openjdk
-
-Download selenium-server-standalone-3.5.3.jar
-
-Download chromedriver 
+*Lưu ý: Selenium khởi tạo 1 google chrome mới để load trang nên không sử dụng trên linux.
--- app/controllers/Admin/WebdriverController.php
+++ app/controllers/Admin/WebdriverController.php
@@ -36,33 +36,35 @@ class WebdriverController extends AppController
 
     public function webdriver(Request $request)
     {
+
         $url = $request->getPost()->get('url');
+        $host = '127.0.0.1:5555';
 
-        // Thiết lập ChromeDriver
-        $chromeDriverPath = '/usr/local/bin/chromedriver';
-        putenv('webdriver.chrome.driver=' . $chromeDriverPath);
-
-        // Khởi tạo DesiredCapabilities cho Chrome
         $capabilities = DesiredCapabilities::chrome();
+        $driver = RemoteWebDriver::create($host, $capabilities);
 
-        // Khởi tạo RemoteWebDriver với địa chỉ IP và cổng
-        $driver = RemoteWebDriver::create("http://192.168.1.219:4444/wd/hub", $capabilities);
-
-        // Mở trang web cần chụp ảnh
+        // Mở trang web
         $driver->get($url);
 
         // Đảm bảo cửa sổ trình duyệt có kích thước đủ lớn để hiển thị toàn bộ trang web
         $window = new WebDriverDimension(1920, 1080);
         $driver->manage()->window()->setSize($window);
 
-        // Chụp ảnh màn hình toàn trang
-        $screenshot = $driver->takeScreenshot();
+        // Delay 3 giây
+        sleep(3);
 
-        // Lưu ảnh vào file
-        $file = rand(1, 10000) . '.png';
-        file_put_contents($file, $screenshot);
+        // Khởi tạo thư mục lưu trữ ảnh
+        $imageDir = 'image';
+        if (!is_dir($imageDir)) {
+            mkdir($imageDir);
+        }
 
-        // Đóng trình duyệt
+        // Tạo tên file và đường dẫn tới thư mục image
+        $filename = $imageDir . '/' . uniqid() . '.png';
+
+        // Chụp màn hình và lưu thành file
+        $driver->takeScreenshot($filename);
+
         $driver->quit();
     }
 }
