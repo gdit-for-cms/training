6700fa87cb83f4a40aeab3a794feb8b19d583d53
GLODEIT\Vu.NL <vu.nl@globaldesignit.vn>
2024-02-29 10:40:31 +0700

    delete file test

--- public/test.php
+++
@@ -1,6 +0,0 @@
-<?php
-$redis = new Redis();
-$redis->connect('192.168.1.217', 6379);
-echo "Connection to server successfully";
-// Check whether server is running or not
-echo "Server is running: " . $redis->ping();
