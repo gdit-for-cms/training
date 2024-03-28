16b234ad55ad6366af0749744f387d4cae1a3cb9
GLODEIT\Vu.NL <vu.nl@globaldesignit.vn>
2024-03-28 08:58:46 +0700

    fix automatic selenium

--- app/models/Store.php
+++ app/models/Store.php
@@ -9,8 +9,8 @@ use Exception;
 use Core\QueryBuilder;
 
 class Store extends Model {
-
     use QueryBuilder;
+    static $abc = 1;
 
     private $_table = 'store';
 
@@ -76,24 +76,33 @@ class Store extends Model {
         if (!strpos($url, "shopeefood")) {
             return -1;
         }
-        if (checkSelenium() == 1) {
+
+
+        if (getStatusCrawling() == 1) {
             $_SESSION['failed_create'] = 'failed_create';
             header('Location: /meal/create');
             exit;
         }
 
-        if (checkSelenium() == 0) {
+        $ssh = createConnection();
+        if ($ssh === 0) {
             $_SESSION['failed_connect_selenium'] = 'failed_connect_selenium';
             header('Location: /meal/create');
             exit;
         }
+        $check = checkSelenium($ssh);
+        if ($check == 1) {
+            $_SESSION['failed_create'] = 'failed_create';
+            header('Location: /meal/create');
+            exit;
+        }
 
-        runSelenium();
+        runSelenium($ssh);
         $store = new Store();
         $pageSource = getHTMLPage($url);
         if ($pageSource == -1) {
             $_SESSION['failed_create'] = 'failed_create';
-            stopSelenium();
+            stopSelenium($ssh);
             header('Location: /meal/create');
             exit;
         }
@@ -102,7 +111,7 @@ class Store extends Model {
 
         $options = LIBXML_NOERROR | LIBXML_NOWARNING;
         $dom = new DOMDocument();
-        stopSelenium();
+        stopSelenium($ssh);
         try {
             $dom->loadHTML('<?xml encoding="utf-8" ?>' . $pageSource, $options);
         } catch (Exception $e) {
